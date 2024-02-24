0441f50b049856b6db30c9e3c62f17d61cfe61a2
unknown <assaadchanelle@gmail.com>
2024-02-24 09:23:02 +0200

    first commit

---
+++ src/data/NavigationData.tsx
@@ -0,0 +1,9 @@
+const navigation = [
+    { name: "Home", href: "#home" },
+    { name: "About Me", href: "#about" },
+    { name: "Projects", href: "#projects" },
+    { name: "Contact", href: "#contact" },
+  ];
+  
+  export default navigation;
+  
